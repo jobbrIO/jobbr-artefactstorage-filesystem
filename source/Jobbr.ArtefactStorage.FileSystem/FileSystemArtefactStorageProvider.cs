3359f7024c7dc8ab654e0819018e58ec2682e862
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-01-28 13:52:48 +0100

    extracted file system artefact storage provider from server

---
+++ source/Jobbr.ArtefactStorage.FileSystem/FileSystemArtefactStorageProvider.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Jobbr.ComponentModel.ArtefactStorage;
+
+namespace Jobbr.ArtefactStorage.FileSystem
+{
+    /// <summary>
+    /// Stores Jobbr artefacts in the file system.
+    /// </summary>
+    public class FileSystemArtefactsStorageProvider : IArtefactsStorageProvider
+    {
+        private readonly string dataDirectory;
+
+        public FileSystemArtefactsStorageProvider(string dataDirectory)
+        {
+            this.dataDirectory = dataDirectory;
+        }
+
+        public override string ToString()
+        {
+            return $"[{this.GetType().Name}, DataDirectory: '{Path.GetFullPath(this.dataDirectory)}']";
+        }
+
+        public void Save(string container, string fileName, Stream content)
+        {
+            var dir = Directory.CreateDirectory(Path.Combine(this.dataDirectory, container));
+            var fileFullPath = Path.Combine(dir.FullName, fileName);
+
+            if (File.Exists(fileFullPath))
+            {
+                File.Delete(fileFullPath);
+            }
+
+            using (var fileStream = File.Create(fileFullPath))
+            {
+                content.CopyTo(fileStream);
+            }
+        }
+
+        public Stream Load(string container, string fileName)
+        {
+            var dir = Directory.CreateDirectory(Path.Combine(this.dataDirectory, container));
+            var fileFullPath = Path.Combine(dir.FullName, fileName);
+
+            if (File.Exists(fileFullPath))
+            {
+                return File.OpenRead(fileFullPath);
+            }
+
+            return null;
+        }
+
+        public List<FileInfo> GetFiles(string container)
+        {
+            var dir = Directory.CreateDirectory(Path.Combine(this.dataDirectory, container));
+
+            return dir.GetFiles().ToList();
+        }
+    }
+}
